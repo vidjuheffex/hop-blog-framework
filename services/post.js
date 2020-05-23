f9e25ceaf0bcf3df4e9caae933994867f6b8e833
julian herrera <julian.herrera@live.com>
2020-05-23 04:09:53 -0500

    add prism for syntax highlighting

--- services/post.js
+++ services/post.js
@@ -6,6 +6,9 @@ service post (o) {
   const p = getPostByUrl(o.title)
   
   return <sitewrap>
+           <link rel="stylesheet" type="text/css" href="https://cdn.jsdelivr.net/npm/prismjs@1.20.0/themes/prism.css" />
+           <script src="https://cdn.jsdelivr.net/npm/prismjs@1.20.0/prism.min.js" />
+           <script src="https://cdn.jsdelivr.net/npm/prismjs@1.20.0/plugins/autoloader/prism-autoloader.min.js" />
            <div class="mb-5">
              <h1 class="display-4">${p.title}</h1>
              <h6 class="text-muted">${p.dateUpdated}</h6>
