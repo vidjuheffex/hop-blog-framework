e8f282971c6828d521176bc43674a9506433b0be
julian herrera <julian.herrera@live.com>
2020-05-25 22:35:31 -0500

    add favicon

--- elements/PAGEHEAD.js
+++ elements/PAGEHEAD.js
@@ -12,6 +12,15 @@ const PAGEHEAD = () =>
               name="viewport"
               content="width=device-width, initial-scale=1, shrink-to-fit=no"
             />
+        <link rel="apple-touch-icon" sizes="180x180" href="/home/jules/Code/website/public/apple-touch-icon.png">
+        <link rel="icon" type="image/png" sizes="32x32" href="/home/jules/Code/website/public/favicon-32x32.png">
+        <link rel="icon" type="image/png" sizes="16x16" href="/home/jules/Code/website/public/favicon-16x16.png">
+        <link rel="manifest" href="/home/jules/Code/website/public/site.webmanifest">
+        <link rel="mask-icon" href="/home/jules/Code/website/public/safari-pinned-tab.svg" color="#5bbad5">
+        <link rel="shortcut icon" href="/home/jules/Code/website/public/favicon.ico">
+        <meta name="msapplication-TileColor" content="#2b5797">
+        <meta name="msapplication-config" content="/home/jules/Code/website/public/browserconfig.xml">
+        <meta name="theme-color" content="#ffffff">
         </head>)
 
 module.exports = PAGEHEAD;
